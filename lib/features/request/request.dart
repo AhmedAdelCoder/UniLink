3d36c3d9306ce8b51a0cd4d656d1b9cbb15e1f45
mayermagdy <mayerzakaria05@gmail.com>
2026-03-28 13:07:49 +0200

    make the vercel prod

--- lib/features/request/request.dart
+++ lib/features/request/request.dart
@@ -1,21 +1,28 @@
-
-
+// request.dart
 // ignore: depend_on_referenced_packages
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Future<void> sendWelcomeEmail(String email, String name) async {
-  try 
-  {
-    await http.post
-    (
-      Uri.parse("http://192.168.1.4:3000/send-email"), // use this if testing on emulator
+  try {
+    final response = await http.post(
+      Uri.parse(
+        "https://unilink-83tn12duo-mayerzakarias-projects.vercel.app/api/sendemails",
+      ),
       headers: {"Content-Type": "application/json"},
       body: jsonEncode({
         "email": email,
         "name": name,
       }),
     );
+
+    if (response.statusCode == 200) {
+      // ignore: avoid_print
+      print("Welcome email sent!");
+    } else {
+      // ignore: avoid_print
+      print("Failed to send email: ${response.body}");
+    }
   } catch (e) {
     // ignore: avoid_print
     print("Error sending email: $e");
